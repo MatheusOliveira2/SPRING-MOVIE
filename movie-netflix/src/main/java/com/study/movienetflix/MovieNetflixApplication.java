75d97beba281c1947b72ea73012c43490f6f5411
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-20 18:00:09 -0300

    API Start

---
+++ movie-netflix/src/main/java/com/study/movienetflix/MovieNetflixApplication.java
@@ -0,0 +1,13 @@
+package com.study.movienetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieNetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieNetflixApplication.class, args);
+	}
+
+}
---
+++ movie-netflix/src/test/java/com/study/movienetflix/MovieNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.movienetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieNetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
