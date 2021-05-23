3ecaed5b346988bade2837d2b2d222c11562796c
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-23 12:36:43 -0300

    Validation

--- movie-netflix/src/main/java/com/study/movienetflix/controllers/CategoryController.java
+++ movie-netflix/src/main/java/com/study/movienetflix/controllers/CategoryController.java
@@ -3,9 +3,13 @@ package com.study.movienetflix.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.movienetflix.model.entities.Category;
@@ -19,12 +23,20 @@ public class CategoryController {
 	private CategoryRepository categoryRepository;
 	
 	@PostMapping
-	public Category newCategory(@Valid @RequestBody Category category) {
+	public Category newCategory(@RequestBody @Valid Category category) {
 		this.categoryRepository.save(category);
 		return category;
 	}
 	
+	@GetMapping
+	@ResponseBody
 	public Iterable<Category> getCategories() {
 		return this.categoryRepository.findAll();
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	@ResponseBody
+	public void removeCategory(@PathVariable int id) {
+		this.categoryRepository.deleteById(id);
 	}
 }
