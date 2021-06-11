668deb74c5a697cd7dca0d941d5df3d7e5ca47c7
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-06-10 21:56:14 -0300

    Exception Handler

---
+++ movie-netflix/src/main/java/com/study/movienetflix/util/MessageUtils.java
@@ -0,0 +1,7 @@
+package com.study.movienetflix.util;
+
+public abstract class MessageUtils {
+
+    public static final String CATEGORY_ALREADY_EXISTS = "Category already exists in the database";
+    public static final String NO_RECORD_FOUND = "No records found";
+}
